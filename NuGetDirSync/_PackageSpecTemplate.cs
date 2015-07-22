ba19a26625ad9f1885fadde8d3552e66a0a4dcee
Courtney de Lautour <courtney.deLautour@rwwa.com.au>
2015-07-22 16:33:35 +0800

    update to use Rx

---
+++ NuGetDirSync/_PackageSpecTemplate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace NuGetDirSync
+{
+    public partial class PackageSpecTemplate : PackageSpecTemplateBase
+    {
+        public readonly Version Version;
+        public readonly string PackageName;
+
+        public PackageSpecTemplate(Version version, string packageName)
+        {
+            Version = version;
+            PackageName = packageName;
+        }
+    }
+}
